adb0724dd8237f5f23ac32208d2a49950b7d40b9
Osamu Ikehara <ikehara@corp.joes.co.jp>
2017-02-28 17:07:42 +0900

    change .user.ini to php.ini

--- htaccesstophpini.sh
+++ htaccesstophpini.sh
@@ -3,8 +3,8 @@
 for user in `/bin/ls /var/cpanel/users`;do
   for f in `find /home/${user}/public_html -type f -name \.htaccess|xargs grep -l "^php_"`;do
     d=`echo $f|sed -e 's/\.htaccess//'`
-    grep "^php_" $f|awk '{print $2" = "$3}' > ${d}.user.ini
-    chown $user:$user ${d}.user.ini
+    grep "^php_" $f|awk '{print $2" = "$3}' > ${d}php.ini
+    chown $user:$user ${d}php.ini
     sed -i -pe 's/^php_/#php_/' $f
   done
 done
