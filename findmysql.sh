8ac7c8666d3a5431f03cd570d10810462818620f
Osamu Ikehara <ikehara@corp.joes.co.jp>
2017-03-01 19:19:17 +0900

    findmysql changed format

--- findmysql.sh
+++ findmysql.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+TMPFILE="/tmp/mysql4tempfile.txt"
+rm -f $TMPFILE
 for db in `mysqlshow |grep _|cut -d\  -f 2`;do
-	user=`echo $db|cut -d_ -f1`
-	echo -n "$user,$db,"
-	find /home/${user}/public_html -type f -print0|xargs -0 grep -l $db
+        user=`echo $db|cut -d_ -f1`
+        find /home/${user}/public_html -type f -print0|xargs -0 grep -Ho $db | uniq >> $TMPFILE
+done
+
+for l in `cat $TMPFILE`;do
+        f=`echo $l|cut -d: -f1`
+        db=`echo $l|cut -d: -f2`
+        echo "db=$db, file is $f"
 done
