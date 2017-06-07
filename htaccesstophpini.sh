53d8241654d0646b8605e900596a3951e67221d3
Osamu Ikehara <ikehara@corp.joes.co.jp>
2017-06-07 12:26:44 +0900

    fix bug on htaccesstophpini

--- htaccesstophpini.sh
+++ htaccesstophpini.sh
@@ -2,9 +2,10 @@
 
 for user in `/bin/ls /var/cpanel/users`;do
   for f in `find /home/${user}/public_html -type f -name \.htaccess|xargs grep -l "^php_"`;do
-    d=`echo $f|sed -e 's/\.htaccess//'`
-    grep "^php_" $f|awk '{print $2" = "$3}' > ${d}php.ini
-    chown $user:$user ${d}php.ini
+    d=`dirname $f`
+    grep "^php_" $f|awk '{print $2" = "$3}' > ${d}/php.ini
+    chown $user:$user ${d}/php.ini
     sed -i -e 's/^php_/#php_/' $f
+#    echo "suPHP_ConfigPath $d" >> $f
   done
 done
