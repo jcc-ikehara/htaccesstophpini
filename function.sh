416257456fc2c47d8d35cfeaae306b497b421da6
Osamu Ikehara <ikehara@corp.joes.co.jp>
2017-06-07 12:49:10 +0900

    未使用

--- function.sh
+++ function.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 genpass() {
-	[ -n $1 ] && len=$1 || len=16
-	cat /dev/urandom|tr -dc 'a-zA-Z0-9'|fold -w $len|head -n 1
+        len=12
+        [ $# -eq 1 ] && [ $1 -gt 1 ] && len=$1
+        cat /dev/urandom|tr -dc 'a-zA-Z0-9'|fold -w $len|head -n 1
 }
